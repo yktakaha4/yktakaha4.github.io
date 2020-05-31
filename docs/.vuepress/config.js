045db5120e560bf8168f9a233165cfd2d4a0c300
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2020-05-31 11:31:44 +0900

    add config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'yktakaha4.github.io',
+  description: '',
+  dest: './dist',
+  extraWatchFiles: [
+    '**/*'
+  ],
+  head: [
+    [
+      'link', {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Noto+Sans+JP'
+      }
+    ]
+  ],
+  themeConfig: {
+    navbar: false,
+    sidebar: false,
+  }
+};
