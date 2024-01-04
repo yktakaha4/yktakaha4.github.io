2dcc2c8d42be506494541418880f9183f03c7794
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-01-04 22:39:44 +0900

    fix

--- export_pdf.bash
+++ export_pdf.bash
@@ -3,12 +3,13 @@
 set -euo pipefail
 
 pdf_file_path="$(cd "$(dirname "$0")";pwd)/pdf/out/resume.pdf"
+chmod 0777 "$(dirname "$pdf_file_path")"
 
 echo "remove old pdf file..."
 rm -f "$pdf_file_path"
 
 echo "generate pdf file..."
-docker compose -f docker-compose.pdf.yml up
+docker compose -f docker-compose.pdf.yml up -d
 docker compose -f docker-compose.pdf.yml wait puppeteer
 
 ls -l "$pdf_file_path"
