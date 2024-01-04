434d5caf82e00071f0e3bece05393f7a112d74cf
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-01-04 16:06:33 +0900

    add profile images

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
     },
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
+        "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/src/jest/fileMock.ts",
     },
     collectCoverage: true,
     collectCoverageFrom: [
---
+++ src/components/ProfileImages.tsx
@@ -0,0 +1,12 @@
+import {FC} from "react";
+import {Avatar} from "@/components/ui/Avatar";
+
+export const ProfileImages: FC = () => {
+    const src = [
+        require('@site/static/img/icon.jpg').default,
+        require('@site/static/img/yktakaha4.jpg').default,
+    ]
+    return (<Avatar
+        src={src}
+    />)
+}
