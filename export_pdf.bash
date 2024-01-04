303f8d72aa9f2ffcf26af3aca40cec7a40320038
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-01-04 22:20:14 +0900

    refactor scripts

--- export_pdf.bash
+++ export_pdf.bash
@@ -9,16 +9,8 @@ rm -f "$pdf_file_path"
 
 echo "generate pdf file..."
 docker compose -f docker-compose.pdf.yml up -d
+docker compose -f docker-compose.pdf.yml wait puppeteer
 
-while [ ! -e "$pdf_file_path" ]; do
-  echo "waiting for pdf file..."
-  sleep 3
-done
-
-echo "pdf file generated!"
 ls -l "$pdf_file_path"
-
-echo "remove docker container..."
-docker compose -f docker-compose.pdf.yml down
 
 echo "done!"
