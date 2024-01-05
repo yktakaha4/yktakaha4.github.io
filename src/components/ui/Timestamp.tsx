b7dfd445b779a71791fac342bbebf2e325b0433b
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-01-06 01:43:54 +0900

    fix format

--- src/components/ui/Timestamp.tsx
+++ src/components/ui/Timestamp.tsx
@@ -1,9 +1,6 @@
-import {FC} from "react";
-import dayjs from "dayjs";
-
+import { FC } from 'react';
+import dayjs from 'dayjs';
 
 export const Timestamp: FC = () => {
-    return (
-        <>内容は{dayjs().format('YYYY/M/D')}時点の情報です</>
-    );
+  return <>内容は{dayjs().format('YYYY/M/D')}時点の情報です</>;
 };
