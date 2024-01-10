83af95a0a4c7e0442707a0ed6eeef67df1c89266
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-01-11 01:35:14 +0900

    fix icon images

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,17 +1,38 @@
 import React, { FC } from 'react';
 import Link from '@docusaurus/Link';
 
-export type AvatarProps = {
-  src: string | Array<string>;
+export type Src = {
+  src: string;
+  width?: string;
+  height?: string;
   alt?: string;
+  srcSets?: Array<{
+    src: string;
+    width: string;
+  }>;
+  sizes?: Array<{
+    size: string;
+  }>;
+  sources?: Array<{
+    type: string;
+    srcset: string;
+  }>;
+};
+
+export type AvatarProps = {
+  src: Src | Array<Src>;
   name?: string;
   subTitle?: string;
   loading?: 'lazy' | 'eager';
 };
 
 export const Avatar: FC<AvatarProps> = ({ src, name, subTitle, loading }) => {
-  const alt = 'アバター画像';
   const srcList = Array.isArray(src) ? src : [src];
+
+  const alt = ({ alt }: Src) => alt || 'アバター画像';
+  const srcSet = ({ srcSets }: Src) =>
+    srcSets?.map(({ src, width }) => `${src} ${width}`).join(',');
+  const sizes = ({ sizes }: Src) => sizes?.map(({ size }) => size).join(',');
 
   return (
     <div className="avatar">
@@ -20,9 +41,23 @@ export const Avatar: FC<AvatarProps> = ({ src, name, subTitle, loading }) => {
           key={i}
           className="avatar__photo-link avatar__photo avatar__photo--lg"
           target="_blank"
-          to={src}
+          to={src.src}
         >
-          <img title={alt} loading={loading} alt={alt} src={src} />
+          <picture>
+            {src.sources?.map(({ type, srcset }, i) => (
+              <source key={i} type={type} srcSet={srcset} />
+            ))}
+            <img
+              title={alt(src)}
+              loading={loading}
+              alt={alt(src)}
+              src={src.src}
+              srcSet={srcSet(src)}
+              sizes={sizes(src)}
+              width={src.width}
+              height={src.height}
+            />
+          </picture>
         </Link>
       ))}
       <div className="avatar__intro">
