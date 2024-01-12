d2a16d257b7d558892197f94fbed2b7fcb246aa7
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2024-01-13 01:46:33 +0900

    warn directory not found

--- src/services/scripts/fetchZennTopics.ts
+++ src/services/scripts/fetchZennTopics.ts
@@ -5,9 +5,18 @@ import { logger } from '@/services/logging';
 export const fetchZennTopics = async () => {
   logger.info('start');
   const directoryPath = getZennContentArticlesDirectoryPath();
-  const topics = await scrapeTopics(directoryPath);
-  logger.info('total', { count: topics.length });
-  await storeTopics(topics);
+  try {
+    const topics = await scrapeTopics(directoryPath);
+    logger.info('total', { count: topics.length });
+
+    await storeTopics(topics);
+  } catch (e) {
+    if (e instanceof Error && e.message.startsWith('Directory not found:')) {
+      logger.warn(e.message);
+    } else {
+      throw e;
+    }
+  }
   logger.info('end');
 };
 
