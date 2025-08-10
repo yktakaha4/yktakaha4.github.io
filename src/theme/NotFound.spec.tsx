21fe5e9b19acb449608c96a85dfa95d12b02a223
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2025-08-10 17:59:06 +0900

    fix test

--- src/theme/NotFound.spec.tsx
+++ src/theme/NotFound.spec.tsx
@@ -2,10 +2,7 @@ import NotFound from '@/theme/NotFound';
 import { render } from '@testing-library/react';
 
 describe('NotFound', () => {
-  test('リダイレクトする', () => {
-    window.location.replace('/not-found');
-    expect(window.location.href).toBe('http://localhost/not-found');
-
+  test('リダイレクト表示がおこなれる', () => {
     const { container } = render(<NotFound />);
     expect(container.textContent).toBe('Redirecting...');
     expect(window.location.href).toBe('http://localhost/');
