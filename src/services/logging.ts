a3a31784e7c0b56b87a8d74a626b3234df326c72
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2025-08-13 22:29:05 +0900

    fix logging test

--- src/services/logging.ts
+++ src/services/logging.ts
@@ -2,28 +2,30 @@ import log4js from 'log4js';
 import util from 'util';
 import * as process from 'process';
 
+export const singleLine = (logEvent: { data: Array<unknown> }) => {
+  return logEvent.data
+    .map((d) => {
+      if (
+        typeof d === 'boolean' ||
+        typeof d === 'number' ||
+        typeof d === 'string'
+      ) {
+        return d.toString().replace(/\n/gm, '\\n');
+      } else {
+        return util
+          .inspect(d, { breakLength: Infinity })
+          .replace(/\n/gm, '\\n');
+      }
+    })
+    .filter((d) => d.length > 0)
+    .join(' ');
+};
+
 const logLayout = {
   type: 'pattern',
   pattern: '%[%d %p %f{1}:%l %x{singleLine}%]',
   tokens: {
-    singleLine: function (logEvent: { data: Array<unknown> }) {
-      return logEvent.data
-        .map((d) => {
-          if (
-            typeof d === 'boolean' ||
-            typeof d === 'number' ||
-            typeof d === 'string'
-          ) {
-            return d.toString().replace(/\n/gm, '\\n');
-          } else {
-            return util
-              .inspect(d, { breakLength: Infinity })
-              .replace(/\n/gm, '\\n');
-          }
-        })
-        .filter((d) => d.length > 0)
-        .join(' ');
-    },
+    singleLine,
   },
 };
 
